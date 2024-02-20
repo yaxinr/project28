101e97717259c2bb1554d996be7f99f9f79b2a2a
Gilfanov Ramil <yaxinr@gmail.com>
2024-02-20 23:45:47 +0500

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,55 @@
+﻿interface IHandler
+{
+    TimeSpan Timeout { get; }
+
+    Task PerformOperation(CancellationToken cancellationToken);
+}
+
+class Handler : IHandler
+{
+    private readonly IConsumer _consumer;
+    private readonly IPublisher _publisher;
+    private readonly ILogger<Handler> _logger;
+
+    public TimeSpan Timeout { get; }
+
+    public Handler(
+      TimeSpan timeout,
+      IConsumer consumer,
+      IPublisher publisher,
+      Ilogger<Handler> logger)
+    {
+        Timeout = timeout;
+
+        _consumer = consumer;
+        _publisher = publisher;
+        _logger = logger;
+    }
+
+    public Task PerformOperation(CancellationToken cancellationToken)
+    {
+        //TODO: place code here
+
+        return Task.CompletedTask;
+    }
+}
+
+record Payload(string Origin, byte[] Data);
+record Address(string DataCenter, string NodeId);
+record Event(IReadOnlyCollection<Address> Recipients, Payload Payload);
+
+enum SendResult
+{
+    Accepted,
+    Rejected
+}
+
+interface IConsumer
+{
+    Task<Event> ReadData();
+}
+
+interface IPublisher
+{
+    Task<SendResult> SendData(Address address, Payload payload);
+}
