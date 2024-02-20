eabf5e6a84387e9f39b1f9733ebdf3d717280e60
Gilfanov Ramil <yaxinr@gmail.com>
2024-02-21 00:26:35 +0500

    handler PerformOperation

--- Class1.cs
+++ Class1.cs
@@ -9,27 +9,47 @@ class Handler : IHandler
 {
     private readonly IConsumer _consumer;
     private readonly IPublisher _publisher;
-    private readonly ILogger<Handler> _logger;
+    //private readonly ILogger<Handler> _logger;
 
     public TimeSpan Timeout { get; }
 
     public Handler(
       TimeSpan timeout,
       IConsumer consumer,
-      IPublisher publisher,
-      Ilogger<Handler> logger)
+      IPublisher publisher
+        //,      Ilogger<Handler> logger
+        )
     {
         Timeout = timeout;
 
         _consumer = consumer;
         _publisher = publisher;
-        _logger = logger;
+        //_logger = logger;
     }
 
     public Task PerformOperation(CancellationToken cancellationToken)
     {
         //TODO: place code here
-
+        //while (true)
+        {
+            var t = _consumer.ReadData();
+            t.Wait(cancellationToken);
+            var event1 = t.Result;
+            foreach (var address in event1.Recipients)
+            {
+                var task = Task.Run(() =>
+                {
+                    while (true)
+                    {
+                        var sendTask = _publisher.SendData(address, event1.Payload);
+                        sendTask.Wait();
+                        if (sendTask.Result == SendResult.Accepted) break;
+                        Thread.Sleep((int)this.Timeout.TotalMilliseconds);
+                    }
+                });
+                task.Start();
+            }
+        }
         return Task.CompletedTask;
     }
 }
